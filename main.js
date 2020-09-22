913b0c1a33e55d063f279f4ec5f6a96869718718
Alex Bernal <alexitobt01@gmail.com>
2020-09-22 20:12:37 +0200

    Ejercicio 1-c

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const ALERT_MESSAGE=("Soy el primer script");
+nombre_navegador= navigator.appName;
+version_navegador = navigator.appVersion; 
+
+
+alert(ALERT_MESSAGE+"y estoy funcionando sobre "+ nombre_navegador+version_navegador);
