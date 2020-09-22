fb665dd81d38f2810ce872740318286f9134895b
Alex Bernal <alexitobt01@gmail.com>
2020-09-22 21:02:17 +0200

    Ejercicio 2 Forma de Tabla

--- AEV1_2.js
+++ AEV1_2.js
@@ -1,9 +1,19 @@
 var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-for (const value of meses) {
-    document.write(value);
-    document.write("<br>");
-  }
+
+    for (let j = 0; j < meses.length; j++) {
+        document.write(meses[j]);
+        document.write("\n");
+       
+
+        if (j==3 || j==7) {
+            document.write("<br>");
+            document.write("<br>");
+        }
+        
+    }
+    
+
 
 
   
