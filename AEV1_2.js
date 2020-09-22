26d022f2957af0be8bef9f63f5053d8999497e15
Alex Bernal <alexitobt01@gmail.com>
2020-09-22 20:54:12 +0200

    Ejercicio 2 Lineas Consecutivas

---
+++ AEV1_2.js
@@ -0,0 +1,9 @@
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for (const value of meses) {
+    document.write(value);
+    document.write("<br>");
+  }
+
+
+  
