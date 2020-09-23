460a2987e3d366e1106e4ea89ff67cbfec12d28f
Alex Bernal <alexitobt01@gmail.com>
2020-09-23 10:54:40 +0200

    Ejercicio 3

---
+++ AEV1_3.js
@@ -0,0 +1,7 @@
+const values = [true, false, 5, "hola", [1,2,3], {age: 2, gender: 'male'}];
+
+for (let i = 0; i < values.length; i++) {
+    var tipo=values[i];
+    document.write(typeof tipo);
+    document.write("<br>");
+}
