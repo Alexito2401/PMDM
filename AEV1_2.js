2c7f69d6b11ed1ac825835217cf64a34ef2094b2
Alex Bernal <alexitobt01@gmail.com>
2020-09-24 21:13:41 +0200

    arreglo del AEV1_2.js

--- AEV1_2.js
+++ AEV1_2.js
@@ -1,17 +1,7 @@
-var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var meses = [["Enero", "Febrero", "Marzo", "Abril"],[ "Mayo", "Junio", "Julio", "Agosto"] ["Septiembre", "Octubre", "Noviembre", "Diciembre"]];
 
 
-    for (let j = 0; j < meses.length; j++) {
-        document.write(meses[j]);
-        document.write("\n");
-       
-
-        if (j==3 || j==7) {
-            document.write("<br>");
-            document.write("<br>");
-        }
-        
-    }
+  console.table(meses)
     
 
 
