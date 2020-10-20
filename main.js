7b64d1d02820b870a0358c4e8176b5ef23079787
Alexito2401 <57366225+Alexito2401@users.noreply.github.com>
2020-10-20 18:59:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -37,16 +37,3 @@ function changeImg() {
     }
 
 }
-
-
-function changeColor(x)
-{
-    if(x.style.background=="rgb(247, 211, 88)")
-    {
-        x.style.background="#fff";
-    }else{
-        x.style.background="#F7D358";
-    }
-    return false;
-}
-
