8fc171dce36bf10d255ea41e951e89126600479d
Alex Bernal <alexitobt01@gmail.com>
2020-11-08 20:28:17 +0100

    cambios al paso1->paso2 / paso3->paso4

--- main.js
+++ main.js
@@ -1,47 +1,80 @@
-const callBAck = document.getElementsByName("CallBack");
+const callBackUrl = document.getElementById('CallBack');
 
-const validarCall = () =>{
-  if (callBAck.values === "") {
-      
+//validar si la callback esta vacia
+const validarCall = () => {
+  //obteniendo el valor que se puso en campo text del formulario
+  const callBack = document.getElementById('CallBack').value;
+  //la condición
+  if (callBack.length === 0) {
+    alert('La CallBack URL esta vacia');
+  } else {
+    document.getElementById('continuar1').style.display = 'block';
   }
 }
 
-
-window.addEventListener("load", function() {
-  rellenar.codigo.addEventListener("keypress", soloNumeros, false);
+window.addEventListener('load', function () {
+  rellenar.Codigo.addEventListener('keypress', soloNumeros, false);
 });
 
 //Solo permite introducir numeros.
-function soloNumeros(e){
-  var key = window.event ? e.which : e.keyCode;
+const soloNumeros = (e) => {
+  let key = window.event ? e.which : e.keyCode;
   if (key < 48 || key > 57) {
     e.preventDefault();
   }
 }
 
-  function mayus(e) {
-    e.value = e.value.toUpperCase();
+const mayus = (e) => {
+  e.value = e.value.toUpperCase();
 }
 
-function pregunta(){
-  if (confirm('¿Estas seguro de enviar este formulario?')){
-     document.miformulario.submit()
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('rellenar').addEventListener('submit', validarFormulario);
+});
+
+const validarFormulario = (evento) => {
+  evento.preventDefault();
+  var usuario = document.getElementById('usuario').value;
+  if (usuario.length == 0) {
+    alert('No has escrito nada en el usuario');
+    return;
+  }
+  this.submit();
+}
+
+const escribe = () => {
+  escribir = document.getElementById('caja')
+  minombre = '<p> Nombre: ' + document.rellenar.Nombre.value + '</p>'
+  miapellido = '<p> Apellidos: ' + document.rellenar.Apellidos.value + '</p>'
+  mifecha = '<p> Fecha de Nacimiento: ' + document.rellenar.Fecha.value + '</p>'
+  midirrecion = '<p> Dirrecion: ' + document.rellenar.Dirrecion.value + '</p>'
+  micodigo = '<p> Codigo Postal: ' + document.rellenar.Codigo.value + '</p>'
+  miprovincia = '<p> Provincia: ' + document.rellenar.Provincia.value + '</p>'
+  mimunicipio = '<p> Municipio: ' + document.rellenar.Municipio.value + '</p>'
+  escribir.innerHTML = minombre + miapellido + mifecha + midirrecion + micodigo + miprovincia + mimunicipio;
+  window.onload = function () {
+    document.rellenar.ver.onclick = escribe
   }
 }
 
-function escribe() {
-  escribir = document.getElementById("caja")
-  minombre ="<p> Nombre: " + document.rellenar.Nombre.value + "</p>"
-  miapellido = "<p> Apellidos: " + document.rellenar.Apellidos.value + "</p>"
-  mifecha = "<p> Fecha de Nacimiento: " + document.rellenar.Fecha.value + "</p>"
-  midirrecion = "<p> Dirrecion: " + document.rellenar.Dirrecion.value + "</p>"
-  micodigo = "<p> Codigo Postal: " + document.rellenar.codigo.value + "</p>"
-  miprovincia = "<p> Provincia: " + document.rellenar.Provincia.value + "</p>"
-  mimunicipio = "<p> Municipio: " + document.rellenar.Municipio.value + "</p>"
+const continuarPaso1 = () => {
+  var paso1 = document.getElementById('paso1');
+  var paso2 = document.getElementById('paso2');
+  var paso3 = document.getElementById('paso3');
+  var paso4 = document.getElementById('paso4');
 
-
-  escribir.innerHTML = minombre + miapellido + mifecha + midirrecion+ micodigo+ miprovincia+ mimunicipio;
+  if (paso2.style.display !== "none") {
+    paso2.style.display = "inline";
+    paso1.style.display = "none";
   }
-window.onload = function() {
-document.rellenar.ver.onclick = escribe
-} 
+}
+
+const continuarPaso2 = () => {
+  var paso2 = document.getElementById('paso2');
+  var paso3 = document.getElementById('paso3');
+
+  if (paso3.style.display !== "none") {
+    paso3.style.display = "inline";
+    paso2.style.display = "none";
+  }
+}
