7f3b88740a1d0d0b2db023e4f677f8f23bb862db
Alex Bernal <alexitobt01@gmail.com>
2020-11-08 22:21:36 +0100

    haciendo paso 4

--- main.js
+++ main.js
@@ -32,14 +32,48 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('rellenar').addEventListener('submit', validarFormulario);
 });
 
-const validarFormulario = (evento) => {
-  evento.preventDefault();
-  var usuario = document.getElementById('usuario').value;
-  if (usuario.length == 0) {
-    alert('No has escrito nada en el usuario');
-    return;
+function Comprobar() {
+  let nombre = document.getElementById('Nombre').value;
+  let apellido = document.getElementById('Apellidos').value;
+  let fecha = document.getElementById('Fecha').value;
+  let codigo = document.getElementById('Codigo').value;
+  let provincia = document.getElementById('Provincia').value;
+  let municipio = document.getElementById('Municipio').value;
+
+  if (nombre.length == 0) {
+    return false;
   }
-  this.submit();
+  if (apellido.length == 0) {
+    return false;
+  }
+  if (fecha.length == 0) {
+    return false;
+  }
+  if (codigo.length == 0) {
+    return false;
+  }
+  if (provincia.length == 0) {
+    return false;
+  }
+  if (municipio.length == 0) {
+    return false;
+  }
+  return true;
+}
+
+const pregunta = () => {
+  if (Comprobar()){
+    if (confirm("Son los datos introducidos correctos?")) {
+      document.getElementById('continuar2').style.display = 'block';
+      return true;
+    }
+    else {
+      return false;
+    }
+  } else{
+    alert('No se ha completado la informacion');
+  }
+  
 }
 
 const escribe = () => {
@@ -57,11 +91,13 @@ const escribe = () => {
   }
 }
 
+const mostrarCallback = () =>{
+  let call = document.getElementById("CallBack_Paso4");
+}
+
 const continuarPaso1 = () => {
   var paso1 = document.getElementById('paso1');
   var paso2 = document.getElementById('paso2');
-  var paso3 = document.getElementById('paso3');
-  var paso4 = document.getElementById('paso4');
 
   if (paso2.style.display !== "none") {
     paso2.style.display = "inline";
@@ -76,5 +112,15 @@ const continuarPaso2 = () => {
   if (paso3.style.display !== "none") {
     paso3.style.display = "inline";
     paso2.style.display = "none";
+  }
+}
+
+const continuarPaso3 = () => {
+  var paso3 = document.getElementById('paso3');
+  var paso4 = document.getElementById('paso4');
+
+  if (paso4.style.display !== "none") {
+    paso4.style.display = "inline";
+    paso3.style.display = "none";
   }
 }
