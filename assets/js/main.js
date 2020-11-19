9f5d7cbc152b28f929aa51236568775e0ef78ffc
Alex Bernal <albetr@floridauniversitaria.es>
2020-11-19 20:16:01 +0100

    “EXAMEN_1EV mover imagen de arriba abajo y de abajo arriba

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,25 @@ window.addEventListener('mouseout', function (evt) {
         evt.target.style.color = 'black'
     }
 });
+
+const imagen = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg';
+
+const moverArriba = () => {
+    if ( document.getElementById('imagen2').innerHTML == "" && document.getElementById('imagen3').innerHTML != "") {
+        document.getElementById('imagen2').innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg' width='100'/>";
+        document.getElementById('imagen3').innerHTML = "";
+    } else if (document.getElementById('imagen1').innerHTML == ""){
+        document.getElementById('imagen1').innerHTML="<img src='https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg' width='100'/>";
+    document.getElementById('imagen2').innerHTML="";
+    }
+}
+
+const moverAbajo = () => {
+    if ( document.getElementById('imagen2').innerHTML == "" && document.getElementById('imagen1').innerHTML != "") {
+        document.getElementById('imagen2').innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg' width='100'/>";
+        document.getElementById('imagen1').innerHTML = "";
+    } else if (document.getElementById('imagen3').innerHTML == ""){
+        document.getElementById('imagen3').innerHTML="<img src='https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg' width='100'/>";
+    document.getElementById('imagen2').innerHTML="";
+    }
+}
